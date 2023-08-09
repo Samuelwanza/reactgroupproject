94d0ae4bc579013ab561e0e6eff9950cfda61b34
Samuel Wanza <s.wanza@alustudent.com>
2023-08-10 00:58:25 +0200

    Revert "added routing"

--- src/pages/HomeView.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-import React from 'react';
-
-function HomeView() {
-  return <div>HomeView</div>;
-}
-
-export default HomeView;
--- src/pages/ProfileView.jsx
+++
@@ -1,8 +0,0 @@
-/* eslint-disable linebreak-style */
-import React from 'react';
-
-function ProfileView() {
-  return <div>ProfileView</div>;
-}
-
-export default ProfileView;
