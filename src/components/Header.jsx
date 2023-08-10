2508212e37c60889b2f27d09d1242465caa5f5ad
haben mehar <habenmehari71@gmail.com>
2023-08-10 16:41:10 +0300

    header and profile updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Style from './Header.module.css';
+import Logo from '../assets/planet.png';
 
 export default function Header() {
   return (
     <nav className={`${Style.Nav}`}>
-      <Link className={`${Style.Link} ${Style.AppLogo}`} to="/">Space X</Link>
+      <Link className={`${Style.Link} ${Style.AppLogo}`} to="/">
+        <img width="40px" alt="" srcSet={Logo} />
+        Space X
+      </Link>
       <ul className={`${Style.NavItems}`}>
         <li><Link className={`${Style.Link}`} to="/rockets">Rockets</Link></li>
         <li><Link className={`${Style.Link}`} to="/missions">Missions</Link></li>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import MissionsPage from '../pages/missions/missions';
+import ProfilePage from '../pages/profile/Profile';
 
 const routes = () => (
   <Routes>
     <Route path="/missions" element={<MissionsPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
   </Routes>
 );
 
