a1c788aaf8983b8638fbcd9b2f3ce912d64e15ab
Samuel wanza <s.wanza@alustudent.com>
2023-08-10 21:17:37 +0200

    rockets setup

---
+++ src/pages/rockets/Rockets.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Rockets = () => (
+  <div>
+    <img src="" alt="" srcSet="" />
+    <div>
+      <h2>Title</h2>
+      <p>text</p>
+      <button type="button">button</button>
+    </div>
+  </div>
+);
+
+export default Rockets;
